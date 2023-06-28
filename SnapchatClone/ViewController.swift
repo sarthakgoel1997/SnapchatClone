f751a8a4007b70c1cf7e00f1cbcbcba8f52ca14e
Sarthak Goel <sarthak.goel@zomato.com>
2023-06-29 01:24:46 +0530

    Initial Commit

---
+++ SnapchatClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapchatClone
+//
+//  Created by Sarthak Goel on 29/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
